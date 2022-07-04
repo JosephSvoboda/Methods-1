4ca8c06ad8ef0e7c40870d10d942dcba9b11b25d
Joseph Svoboda <joseph.svoboda15@yahoo.com>
2022-07-04 15:30:33 -0400

    first and final commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("What is your name?");
+var name = Console.ReadLine();
+Console.WriteLine($"hello {name}.");
+
+Console.WriteLine("What is your favorite color?");
+var color = Console.ReadLine();
+Console.WriteLine("That's my favorite color as well!");
+
+Console.WriteLine("What is your favorite animal?");
+var animal = Console.ReadLine();
+Console.WriteLine($"I always found {animal} to be fascinating.");
+
+Console.WriteLine($"Last question {name}, what is your favorite band?");
+var band = Console.ReadLine();
+Console.WriteLine($"I've heard a lot about {band} but i never listened to them.");
+
+Console.WriteLine($"So {name} your favorite color is {color}, your favorite animal is {animal} and your favorite band is {band}");
+
+
+
+
